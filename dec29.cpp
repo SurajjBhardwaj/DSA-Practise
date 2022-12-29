d0af9e03ef717eb254dbfa0309aebfd20ba6f681
surajj <pandeyyyysuraj@gmail.com>
2022-12-29 13:18:23 +0530

    solved quick sort

--- dec29.cpp
+++ dec29.cpp
@@ -203,3 +203,90 @@ int main()
     return 0;;
 }
 // } Driver Code Ends
+
+
+//3rd question
+
+//{ Driver Code Starts
+#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    void merge(int arr[], int l, int m, int r)
+    {
+        // Your code here
+        int i = l, j = m+1, ci = 0;
+        int *temp = new int[r - l + 1];
+        while(i <= m && j <= r){
+            if(arr[i] <= arr[j]){
+                temp[ci++] = arr[i++];
+            }else {
+                temp[ci++] = arr[j++];
+            }
+        }
+        while(i <= m ){
+            temp[ci++] = arr[i++];
+        }
+        while(j <= r){
+            temp[ci++] = arr[j++];
+        }
+        for(int k = 0; k < r-l+1; k++){
+            arr[l + k] = temp[k];
+        }
+         
+    }
+    
+    void mergeSort(int arr[], int l, int r)
+    {
+        //code here
+        if(r > l){
+            int m = l + (r - l) /2; 
+            mergeSort(arr, l, m );
+            mergeSort(arr, m + 1, r);
+            merge(arr, l, m, r);
+        }
+        
+        
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+    int n,T,i;
+
+    scanf("%d",&T);
+
+    while(T--){
+    
+    scanf("%d",&n);
+    int arr[n+1];
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+
+    Solution ob;
+    ob.mergeSort(arr, 0, n-1);
+    printArray(arr, n);
+    }
+    return 0;
+}
+// } Driver Code Ends
