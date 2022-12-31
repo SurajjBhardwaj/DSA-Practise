0a45a02c19e94d2cc771155beffb09f3b49fe2ba
surajj <pandeyyyysuraj@gmail.com>
2022-12-31 12:34:07 +0530

    third question

--- dec31.cpp
+++ dec31.cpp
@@ -91,4 +91,62 @@ int main() {
 }
 // } Driver Code Ends
 
+//third question
 
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printDiamond(int n) {
+        // code here
+        //upper part
+         for(int i=0;i<n;i++){
+             
+             for(int k=n-i-1;k>0;k--){
+                 cout<<" ";
+             }
+             
+             for(int j=0;j<=i;j++){
+                 cout<<"* ";
+             }
+             
+             cout<<endl;
+         }
+         
+         //lower part
+         
+         for(int i=0,k=n;i<n;i++,k--){
+             
+             for(int j=0;j<i;j++){
+                 cout<<" ";
+             }
+             for(int s=k;s>0;s--){
+                 cout<<"* ";
+             }
+             cout<<endl;
+             
+             
+         }
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printDiamond(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
