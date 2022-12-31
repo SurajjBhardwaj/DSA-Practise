e23f90866ab67d7c4e6a6fc743d44499e415bd07
surajj <pandeyyyysuraj@gmail.com>
2022-12-31 12:40:39 +0530

    fourth question done

--- dec31.cpp
+++ dec31.cpp
@@ -150,3 +150,53 @@ int main() {
     return 0;
 }
 // } Driver Code Ends
+
+//fourth question:-
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+//Back-end complete function Template for C++
+class Solution{
+public:
+    void printTriangle(int n) {
+        // code here
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                cout<<"* ";
+            }
+            cout<<endl;
+        }
+        
+        for(int i=n;i>1;i--){
+             
+             for(int j=i-1;j>0;j--){
+                 cout<<"* ";
+        }
+            cout<<endl;
+        }
+        
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+//fifth question
