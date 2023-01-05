fc856acc961ba19030325788b0d51e71d4df277c
surajj <pandeyyyysuraj@gmail.com>
2023-01-05 13:14:20 +0530

    added minimum element from stack

--- 2023/jan/5t jan/5jan.cpp
+++ 2023/jan/5t jan/5jan.cpp
@@ -71,3 +71,112 @@ int main()
    }
 }
 // } Driver Code Ends
+
+
+//second question
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+/*
+The structure of the class is as follows
+class _stack{
+stack<int> s;
+int minEle;
+public :
+    int getMin();
+    int pop();
+    void push(int);
+};
+*/
+
+class Solution{
+    int minEle;
+    stack<int> s;
+    stack<int>as;
+    public:
+    
+       /*returns min element from stack*/
+       int getMin(){
+           //Write your code here
+        if(as.empty()==true)
+           return -1;
+           return as.top();
+           
+           
+           
+       }
+       
+       /*returns poped element from stack*/
+       int pop(){
+           
+            if(as.empty()==true){
+               return -1;
+           }
+          if(s.top()==as.top()){
+              as.pop();
+          }
+          int ans=s.top();
+          s.pop();
+          return ans;
+       }
+       
+       /*push element x into the stack*/
+       void push(int x){
+           
+             if(as.empty()==true){
+              s.push(x);
+              as.push(x);
+          }
+          else{
+              s.push(x);
+              if(as.top()>=s.top()){
+                  as.push(x);
+              }
+          }
+           
+       }
+};
+
+//{ Driver Code Starts.
+
+int main()
+ {
+    long long t;
+
+    cin>>t;
+    while(t--)
+    {
+        int q;
+        cin>>q;
+        Solution ob;
+        while(q--){
+           int qt;
+           cin>>qt;
+           if(qt==1)
+           {
+              //push
+              int att;
+              cin>>att;
+              ob.push(att);
+           }
+           else if(qt==2)
+           {
+              //pop
+              cout<<ob.pop()<<" ";
+           }
+           else if(qt==3)
+           {
+              //getMin
+              cout<<ob.getMin()<<" ";
+           }
+       }
+       cout<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
