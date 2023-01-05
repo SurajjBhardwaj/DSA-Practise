a2243b8a8ae5d00737f616ca777b7111552a5ad4
surajj <pandeyyyysuraj@gmail.com>
2023-01-05 13:12:29 +0530

    paranthesis checker using stack

--- 2023/jan/5t jan/5jan.cpp
+++ 2023/jan/5t jan/5jan.cpp
@@ -1,2 +1,73 @@
 //first question
 
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to check if brackets are balanced or not.
+    bool ispar(string s)
+    {
+        // Your code here
+        
+      stack<char>st;
+      int i=0;
+      while(s[i]){
+          
+          if(s[i]=='(' || s[i]=='{' || s[i] =='[' ){
+              st.push(s[i]);
+              i++;
+          }
+          else if(s[i]==')' || s[i]=='}' || s[i]==']'){
+              
+              if(st.empty()){
+                  return false;
+              }
+              if(s[i]==')' && st.top()!='('){
+                  return false;
+              }
+              if(s[i]=='}' && st.top()!='{' ){
+                  return false;
+              }
+              if(s[i]==']' && st.top()!='[' ){
+                  return false;
+              }
+              
+              st.pop();
+              i++;
+          }else{
+              i++;
+          }
+      }
+      
+      
+      return st.empty();
+     
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+   int t;
+   string a;
+   cin>>t;
+   while(t--)
+   {
+       cin>>a;
+       Solution obj;
+       if(obj.ispar(a))
+        cout<<"balanced"<<endl;
+       else
+        cout<<"not balanced"<<endl;
+   }
+}
+// } Driver Code Ends
