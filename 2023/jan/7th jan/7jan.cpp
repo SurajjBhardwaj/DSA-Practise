6519076a99aaf1fc3b408bf9966fc604938ff60d
surajj <pandeyyyysuraj@gmail.com>
2023-01-07 14:52:44 +0530

    added subarray with the given sum

--- 2023/jan/7th jan/7jan.cpp
+++ 2023/jan/7th jan/7jan.cpp
@@ -1,3 +1,4 @@
+//first question
 //{ Driver Code Starts
 #include <bits/stdc++.h>
 
@@ -73,4 +74,81 @@ int main() {
     return 0;
 }
 
+// } Driver Code Ends
+
+//second question
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find a continuous sub-array which adds up to a given number.
+    vector<int> subarraySum(vector<int>arr, int n, long long s)
+    {
+        // Your code here
+        if(s==0){
+            return {-1};
+        }
+        
+      int i=0;
+      int j=0;
+      long long currsum=0;
+      vector<int> v;
+      while(i<n && j<=n){
+          
+          if(currsum<s){
+              currsum+=arr[j];
+               j++;
+          }
+          else if(currsum>s){
+              currsum-=arr[i];
+              i++;
+          }
+          else {
+              v.push_back(i+1);
+              v.push_back(j);
+              return v;
+          } 
+      }
+      v.push_back(-1);
+      return v;
+      
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main()
+ {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        long long s;
+        cin>>n>>s;
+        vector<int>arr(n);
+        // int arr[n];
+        const int mx = 1e9;
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
+        Solution ob;
+        vector<int>res;
+        res = ob.subarraySum(arr, n, s);
+        
+        for(int i = 0;i<res.size();i++)
+            cout<<res[i]<<" ";
+        cout<<endl;
+        
+    }
+	return 0;
+}
 // } Driver Code Ends
