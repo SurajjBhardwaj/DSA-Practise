6c466e77597fb58d6ebad33f609131b903296316
surajj <pandeyyyysuraj@gmail.com>
2023-01-09 11:37:58 +0530

    majority element in an array

--- 2023/jan/9th jan/9jan.cpp
+++ 2023/jan/9th jan/9jan.cpp
@@ -71,4 +71,71 @@ int main()
 
 
 //second question
+//majority element in an array
 
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> Solve(int n, vector<int>& arr) {
+        // Code here
+        
+        vector<int>v;
+        int num1=0,num2=0,c1=0,c2=0;
+        
+        for(int i=0;i<n;i++){
+             
+            if(num1==arr[i]) c1++;
+            else if(num2 == arr[i]) c2++;
+            else if(c1==0) { num1 = arr[i]; c1=1;}
+            else if( c2==0 ) { num2 = arr[i]; c2=1; }
+            else {
+                c1--;
+                c2--;
+            }
+        }
+        
+        c1=0;c2=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]==num1) c1++;
+            if(arr[i]==num2) c2++;
+        }
+        
+        if(c1>n/3)v.push_back(num1);
+        if(c2>n/3)v.push_back(num2);
+        
+        if(v.size()==0){
+            return {-1};
+        }
+        
+        return v;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a;
+        for (int i = 0; i < n; ++i) {
+            int x;
+            cin >> x;
+            a.push_back(x);
+        }
+        Solution obj;
+
+        vector<int> res = obj.Solve(n, a);
+
+        for (auto x : res) cout << x << " ";
+
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
