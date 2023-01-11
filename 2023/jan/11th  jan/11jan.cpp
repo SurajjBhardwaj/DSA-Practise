d86a0047102cd70e71f21a2f803ec21e5c4bc211
surajj <pandeyyyysuraj@gmail.com>
2023-01-11 20:19:09 +0530

    swap two no using xor

---
+++ 2023/jan/11th  jan/11jan.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial template for c++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{   
+public:
+    pair<int, int> get(int a, int b){
+        //complete the function here
+        
+        a=a xor b;
+        b=a xor b;
+        a= a xor b;
+        
+        return { a,b };
+        
+        
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a, b;
+        cin >> a >> b;
+        
+        Solution ob;
+        pair<int, int>p = ob.get(a, b);
+        cout << p.first << ' ' << p.second << endl;
+    
+    }
+    return 0; 
+} 
+
+// } Driver Code Ends
