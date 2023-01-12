d78e38cf7fb48c9f5f3e0d334797e692933b5163
surajj <pandeyyyysuraj@gmail.com>
2023-01-12 22:04:11 +0530

    even odd alternately in ll

--- 2023/jan/12th jan/12jan.cpp
+++ 2023/jan/12th jan/12jan.cpp
@@ -146,3 +146,123 @@ int main()
     return 0;
 }
 // } Driver Code Ends
+
+// 2nd question
+
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+void printList(Node *node)
+{
+    while (node != NULL)
+    {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << "\n";
+}
+
+// } Driver Code Ends
+// User function template for C++
+
+/*
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+class Solution
+{
+public:
+    Node *divide(int N, Node *head)
+    {
+        // code here
+
+        Node *o = new Node(-1);
+        Node *e = new Node(-1);
+
+        Node *even = e;
+        Node *odd = o;
+
+        Node *p = head;
+        while (p)
+        {
+
+            if ((p->data % 2) == 0)
+            {
+                e->next = p;
+                e = e->next;
+            }
+
+            else
+            {
+                // cout<<p->data<<" ";
+                o->next = p;
+                o = o->next;
+            }
+
+            p = p->next;
+        }
+
+        // while(odd){
+        //     cout<<odd->data<<"i ";
+        //     odd = odd->next;
+
+        // }
+
+        e->next = odd->next;
+        o->next = NULL;
+        return even->next;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    // code
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin >> N;
+        int data;
+        cin >> data;
+        struct Node *head = new Node(data);
+        struct Node *tail = head;
+        for (int i = 0; i < N - 1; ++i)
+        {
+            cin >> data;
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+
+        Solution ob;
+        Node *ans = ob.divide(N, head);
+        printList(ans);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
