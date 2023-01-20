252d53da27e2c1d46c1d151ba8073d5890c17ed3
surajj <pandeyyyysuraj@gmail.com>
2023-01-20 22:21:45 +0530

    Maximum Weight Node

--- 2023/jan/20th jan/20jan.cpp
+++ 2023/jan/20th jan/20jan.cpp
@@ -59,3 +59,76 @@ int main()
     }
 }
 // } Driver Code Ends
+
+
+//2nd question
+
+//{ Driver Code Starts
+//Initial Template for C++
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+  public:
+  int maxWeightCell(int N, vector<int> Edge)
+  {
+      // code here
+      
+      if(N==1){
+          
+          if(Edge[0]<0){
+                  return 0;
+          }
+      }
+      
+      else{
+          vector<int>v(N,0);
+          
+          for(int i=0;i<N;i++){
+              
+             if(Edge[i]<0  || Edge[i]>N ){
+                 
+             }
+             else{
+                 
+                v[Edge[i]]+=i;
+                 
+             }
+          }
+          
+          int max=0;
+          int maxi;
+          for(int i=0;i<N;i++){
+              
+              if(v[i]>=max){
+                  
+                  max=v[i];
+                  maxi=i;
+              }
+          }
+          return maxi;
+      }
+  }
+};
+
+//{ Driver Code Starts.
+int main(){
+   int tc;
+   cin >> tc;
+   while(tc--){
+      int N;
+      cin >> N;
+      vector<int> Edge(N);
+      for(int i=0;i<N;i++){
+        cin>>Edge[i];
+      }
+      Solution obj;
+      int ans=obj.maxWeightCell(N, Edge);
+      cout<<ans<<endl;
+   }
+   return 0;
+}
+// } Driver Code Ends
